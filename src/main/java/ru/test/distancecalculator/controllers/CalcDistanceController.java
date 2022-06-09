9aeca8900479fe7bbd1b02707e3a717e18b93017
ilyakpn <70515196+ilyakpn@users.noreply.github.com>
2022-06-09 14:44:15 +0400

    refactoring - add string variable

--- src/main/java/ru/test/distancecalculator/controllers/CalcDistanceController.java
+++ src/main/java/ru/test/distancecalculator/controllers/CalcDistanceController.java
@@ -46,13 +46,20 @@ public class CalcDistanceController {
 
     @GetMapping("/distanceCalculator/calculationResult")
     public ModelAndView calculationResult(String typeOfCalc, long fromCityId, long toCityId) {
+        final String FAILED_MATRIX_DISTANCE = "Расстояние не может быть рассчитано, потому что нет данных в таблице расстояний!";
+        final String FAILED_CITY = "Расстояние не может быть рассчитано, потому что нет данных в таблице городов!";
+
+        final String MATRIX_DISTANCE_RESULT = "Матрица расстояний = %.3f км";
+        final String CROWFLIGHT_RESULT = "Расстояние по прямой = %.3f км";
+
+
         ModelAndView modelAndView = new ModelAndView("calculation-result :: calcResultFragment");
 
         try {
-            String message = "City with id = %d not found";
+            final String CITY_NOT_FOUND = "City with id = %d not found";
 
-            City fromCity = cityRepository.findById(fromCityId).orElseThrow(() -> new EntityNotFoundException(String.format(message, fromCityId)));
-            City toCity = cityRepository.findById(toCityId).orElseThrow(() -> new EntityNotFoundException(String.format(message, toCityId)));
+            City fromCity = cityRepository.findById(fromCityId).orElseThrow(() -> new EntityNotFoundException(String.format(CITY_NOT_FOUND, fromCityId)));
+            City toCity = cityRepository.findById(toCityId).orElseThrow(() -> new EntityNotFoundException(String.format(CITY_NOT_FOUND, toCityId)));
 
             if (typeOfCalc.equals(TypesOfCalc.CROWFLIGHT.getName()) || typeOfCalc.equals(TypesOfCalc.ALL.getName())) {
 
@@ -60,7 +67,7 @@ public class CalcDistanceController {
 
                 double d = crowFlightDistance.calcDistance();
 
-                modelAndView.addObject("crowflightResult", String.format("Расстояние по прямой = %.3f км", d));
+                modelAndView.addObject("crowflightResult", String.format(CROWFLIGHT_RESULT, d));
             }
 
             if (typeOfCalc.equals(TypesOfCalc.DISTANCE_MATRIX.getName()) || typeOfCalc.equals(TypesOfCalc.ALL.getName())) {
@@ -72,20 +79,20 @@ public class CalcDistanceController {
 
                         Distance distance = optionalDistance.get();
 
-                        modelAndView.addObject("distanceMatrix", String.format("Матрица расстояний = %.3f км", distance.getDistance()));
+                        modelAndView.addObject("distanceMatrix", String.format(MATRIX_DISTANCE_RESULT, distance.getDistance()));
 
                     } else {
                         throw new EntityNotFoundException("Distance cannot be calculated, because it is not in the distance table!");
                     }
                 } catch (EntityNotFoundException e) {
 
-                    modelAndView.addObject("exception", "Расстояние не может быть рассчитано, потому что нет данных в таблице расстояний!");
+                    modelAndView.addObject("exception", FAILED_MATRIX_DISTANCE);
 
                 }
             }
 
         } catch (EntityNotFoundException e) {
-            modelAndView.addObject("exception1", "Расстояние не может быть рассчитано, потому что нет данных в таблице городов!");
+            modelAndView.addObject("exception1", FAILED_CITY);
         }
 
         return modelAndView;
